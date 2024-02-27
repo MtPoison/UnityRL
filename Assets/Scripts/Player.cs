317f2aee3a6cc3ecfc1f1bd9d619bec5a0019e20
PoloBongo <earthrp.polo@gmail.com>
2024-02-27 09:00:35 +0100

    move modif

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,9 +1,15 @@
 using UnityEngine;
+using static UnityEditor.Searcher.SearcherWindow.Alignment;
 
 public class Player : MonoBehaviour
 {
     [Header("Movement")]
-    public float moveSpeed;
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float speedJump;
+    [SerializeField] private float RotateSpeed;
+
+    private float horizontal;
+    private float vertical;
 
     public Transform orientation;
 
@@ -18,21 +24,44 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            moveSpeed = 15.0f;
+        }
+        else
+        {
+            moveSpeed = 10.0f;
+        }
     }
 
     private void FixedUpdate()
     {
+        GetAxisRaw();
         MovePlayer();
+        if (Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+    }
+
+    private void GetAxisRaw()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
     }
 
     private void MovePlayer()
     {
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
-
         moveDirection = orientation.forward * vertical + orientation.right * horizontal;
 
         rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
-        transform.Rotate(transform.up, 10 * horizontal);
+        
+        transform.Rotate(transform.up, RotateSpeed * horizontal);
+    }
+
+    private void Jump()
+    {
+        rb.velocity = new Vector3(0, speedJump * Time.deltaTime, 0);
+        transform.up = rb.velocity;
     }
 }
