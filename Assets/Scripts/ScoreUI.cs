86c2abae9c9014d8b4f097d5ee95e3c913c9586a
quentin lf <quentinleforestier0@gmail.com>
2024-02-28 20:20:34 +0100

    fix

--- Assets/Scripts/ScoreUI.cs
+++ Assets/Scripts/ScoreUI.cs
@@ -18,6 +18,6 @@ public class ScoreUI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        scoreText.text = $"{gameManager.scorePlayerA} -- {gameManager.scorePlayerB}";
+        scoreText.text = $"{gameManager.scorePlayerA}  {gameManager.scorePlayerB}";
     }
 }
