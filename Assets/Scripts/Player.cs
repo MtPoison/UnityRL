f6d27ed1aab50b9c80e675cfb71082291bceb4da
PoloBongo <earthrp.polo@gmail.com>
2024-02-29 17:25:48 +0100

    merge tah les ouf enfin finit

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,3 +1,6 @@
+using System.Collections;
+using UnityEditor;
+using UnityEditor.SceneManagement;
 using UnityEngine;
 using static UnityEditor.Searcher.SearcherWindow.Alignment;
 
@@ -7,57 +10,89 @@ public class Player : MonoBehaviour
     [SerializeField] private float moveSpeed;
     [SerializeField, Range(0, 100)] private float intensityJump;
     [SerializeField] private float RotateSpeed;
+    [SerializeField] private float intensityJetpack;
+
+    [SerializeField] private string jumpInputAxis;
     [SerializeField] private string horizontalInputAxis;
     [SerializeField] private string verticalInputAxis;
+    [SerializeField] public string sprintInputAxis;
+    [SerializeField] public string jetpackInputAxis;
 
     private float horizontalPlayerGlobal;
     private float verticalPlayerGlobal;
-    LineRenderer trail;
+    public float sprintPlayerGlobal;
+    public float jetpackPlayerGlobal;
 
+    private bool canJump;
+
+    LineRenderer trail;
     Rigidbody rb;
     Camera cam;
+    SliderManager sliderManager;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
         cam = GetComponentInChildren<Camera>();
         trail = GetComponentInChildren<LineRenderer>();
+        sliderManager = GetComponentInChildren<SliderManager>();
         trail.enabled = false;
     }
 
     private void Update()
     {
         GetAxisRawGlobal();
-
-        if (Input.GetKey(KeyCode.LeftShift))
-        {
-            moveSpeed = 50.0f;
-            cam.fieldOfView = 90.0f;
-            trail.enabled = true;
-        }
-        else
-        {
-            moveSpeed = 15.0f;
-            cam.fieldOfView = 60.0f;
-            trail.enabled = false;
-        }
-
-        if (Input.GetButtonDown("Jump"))
-        {
-            PlayerJump();
-        }
-
     }
 
     private void FixedUpdate()
     {
         MovePlayers();
+
+        if (Input.GetAxisRaw(jumpInputAxis) != 0 && canJump)
+        {
+            PlayerJump();
+        }
+
+        /*        if (sliderManager != null)
+                {
+                    if (Input.GetAxisRaw(jetpackInputAxis) != 0 && sliderManager.GetSlidersValue("JetpackJauge") > 0.0f)
+                    {
+                        PlayerJetpack();
+                    }   
+                }*/
+
+    }
+
+    public void SprintOn()
+    {
+        moveSpeed = 50.0f;
+        cam.fieldOfView = 90.0f;
+        trail.enabled = false;
+    }
+
+    public void SprintOff()
+    {
+        moveSpeed = 15.0f;
+        cam.fieldOfView = 60.0f;
+        trail.enabled = false;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        canJump = true;
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        canJump = false;
     }
 
     private void GetAxisRawGlobal()
     {
         horizontalPlayerGlobal = Input.GetAxisRaw(horizontalInputAxis);
         verticalPlayerGlobal = Input.GetAxisRaw(verticalInputAxis);
+        sprintPlayerGlobal = Input.GetAxisRaw(sprintInputAxis);
+        jetpackPlayerGlobal = Input.GetAxisRaw(jetpackInputAxis);
     }
 
     private void MovePlayers()
@@ -70,5 +105,12 @@ public class Player : MonoBehaviour
     private void PlayerJump()
     {
         rb.AddForce(intensityJump * Time.deltaTime * 1000 * Vector3.up);
+        rb.drag = -1;
+    }
+
+    public void PlayerJetpack()
+    {
+        transform.Rotate(transform.up, RotateSpeed * horizontalPlayerGlobal);
+        rb.AddForce(intensityJump * Time.deltaTime * intensityJetpack * Vector3.up);
     }
 }
---
+++ Assets/Scripts/SliderManager.cs
@@ -0,0 +1,164 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderManager : MonoBehaviour
+{
+    [SerializeField] public string sprintInputAxis;
+    [SerializeField] public string jetpackInputAxis;
+
+    public Slider sprintStaminaSlider;
+    public Slider jetpackStaminaSlider;
+
+    /*public List<Slider> sliders = new List<Slider>();*/
+
+    Player playerComponent;
+
+    public float staminaRemoveValue = 0.1f;
+    public float staminaAddValue = 0.1f;
+
+    public bool isRemoveStamina = false;
+    public bool isRemoveStaminaJetpack = false;
+
+
+
+    private void Start()
+    {
+        /*SliderInitialization();*/
+        Transform parentTransform = transform.parent;
+
+        if (parentTransform != null)
+        {
+            // Si le parent direct existe, r�cup�re le parent de ce parent
+            Transform grandParentTransform = parentTransform.parent;
+
+            if (grandParentTransform != null)
+            {
+                // Si le parent de ce parent existe, on get le Component Player
+                playerComponent = grandParentTransform.GetComponentInChildren<Player>();
+            }
+        }
+        else
+        {
+            Debug.Log("Pas de parent");
+        }
+    }
+
+    /*    private void SliderInitialization()
+        {
+            sliders.AddRange(FindObjectsOfType<Slider>());
+        }*/
+
+    private IEnumerator SmoothChangeSliderValue(Slider slider, float targetValue)
+    {
+        float startValue = slider.value;
+        float sleepTime = 0.0f;
+
+        while (sleepTime < targetValue)
+        {
+            sleepTime += Time.deltaTime;
+            float t = Mathf.Clamp01(sleepTime / targetValue);
+            slider.value = Mathf.Lerp(startValue, targetValue, t);
+            yield return null;
+        }
+
+        slider.value = targetValue;
+    }
+
+    /*public void SetSlidersValue(string tag, float targetValue)
+    {
+        foreach (Slider slider in sliders)
+        {
+            if (slider.CompareTag(tag))
+            {
+                StartCoroutine(SmoothChangeSliderValue(slider, targetValue));
+            }
+        }
+    }*/
+
+    /*    public float GetSlidersValue(string tag)
+        {
+            foreach (Slider slider in sliders)
+            {
+                if (slider.CompareTag(tag))
+                {
+                    return slider.value;
+                }
+            }
+            return 0;
+        }*/
+
+    private void Update()
+    {
+        if (Input.GetAxisRaw(sprintInputAxis) != 0 && sprintStaminaSlider.value > 0.0f)
+        {
+            playerComponent.SprintOn();
+            if (!isRemoveStamina)
+            {
+                StartCoroutine(RemoveStamina(sprintStaminaSlider));
+            }
+        }
+        else
+        {
+            playerComponent.SprintOff();
+            isRemoveStamina = false;
+        }
+
+        if (Input.GetAxisRaw(jetpackInputAxis) != 0 && jetpackStaminaSlider.value > 0.0f)
+        {
+            playerComponent.PlayerJetpack();
+            if (!isRemoveStaminaJetpack)
+            {
+                StartCoroutine(RemoveStaminaJetpack(jetpackStaminaSlider));
+            }
+        }
+        else
+        {
+            isRemoveStaminaJetpack = false;
+        }
+    }
+
+    private IEnumerator RemoveStamina(Slider staminaSlider)
+    {
+        isRemoveStamina = true;
+        while (Input.GetAxisRaw(sprintInputAxis) != 0)
+        {
+            float currentStamina = staminaSlider.value;
+            currentStamina -= staminaRemoveValue * Time.deltaTime;
+            staminaSlider.value = Mathf.Clamp01(currentStamina);
+            yield return null;
+        }
+        isRemoveStamina = false;
+    }
+
+    private IEnumerator RemoveStaminaJetpack(Slider staminaSlider)
+    {
+        isRemoveStaminaJetpack = true;
+        while (Input.GetAxisRaw(jetpackInputAxis) != 0)
+        {
+            float currentStamina = staminaSlider.value;
+            currentStamina -= staminaRemoveValue * Time.deltaTime;
+            staminaSlider.value = Mathf.Clamp01(currentStamina);
+            yield return null;
+        }
+        isRemoveStaminaJetpack = false;
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetAxisRaw(sprintInputAxis) == 0)
+        {
+            float currentStamina = sprintStaminaSlider.value;
+            currentStamina += staminaAddValue * Time.fixedDeltaTime;
+            sprintStaminaSlider.value = Mathf.Clamp01(currentStamina);
+        }
+
+        if (Input.GetAxisRaw(jetpackInputAxis) == 0)
+        {
+            float currentStamina = jetpackStaminaSlider.value;
+            currentStamina += staminaAddValue * Time.fixedDeltaTime;
+            jetpackStaminaSlider.value = Mathf.Clamp01(currentStamina);
+        }
+    }
+}
