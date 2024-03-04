651908a1f60a8a0d1099d08f4ec71b4fe184e994
quentin lf <quentinleforestier0@gmail.com>
2024-03-04 09:02:12 +0100

    menu fini

--- Assets/Scripts/MenuControler.cs
+++ Assets/Scripts/MenuControler.cs
@@ -17,10 +17,9 @@ public class MenuControler : MonoBehaviour
         
     }
 
-    public void ChangeScene()
+    public void ChangeScene( string _sceneName)
     {
-        SceneManager.LoadScene(1);
-        Debug.Log("okk");
+        SceneManager.LoadScene(_sceneName);
     }
 
     public void Quit()
